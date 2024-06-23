be04a70aa9acc7e0d5dfd80fab36cbd5427e51b0
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-23 09:57:55 -0700

    chore: update app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import Form from "./components/Form";
 import Input from "./components/Inputs";
 
 function App() {
+
+  const handleSave = (data: unknown) => {
+    const formData = data as { name: string; age: string };
+
+    console.log(formData);
+  };
+
   return (
     <main>
-      <Form>
+      <Form onSave={handleSave}>
         <Input label="Name" id="name" type="string" />
         <Input label="Age" id="age" type="number" />
       </Form>
