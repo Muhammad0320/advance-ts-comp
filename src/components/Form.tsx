96cbee98c1ab73547d7717475cdda35d51a7021a
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-23 09:54:04 -0700

    chore:  update form status

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,23 @@
-import { ComponentPropsWithoutRef, FC } from "react";
+import { ComponentPropsWithoutRef, FC, FormEvent } from "react";
 
-type FormType = ComponentPropsWithoutRef<"form">;
+type FormType = ComponentPropsWithoutRef<"form"> & {
+  onSave: (value: unknown) => void;
+};
 
-const Form: FC<FormType> = ({ children, ...props }) => {
-  return <form {...props}>{children}</form>;
+const Form: FC<FormType> = ({ children, onSave, ...props }) => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    const formData = new FormData(e.currentTarget);
+
+    const data = Object.fromEntries(formData);
+
+    onSave(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} {...props}>
+      {children}
+    </form>
+  );
 };
 
 export default Form;
