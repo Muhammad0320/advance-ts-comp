7aaf7d17946d0ab94bd239366de35a155034c64c
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-24 04:38:29 +0100

    chore:  update header tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import AButton from "./UI/AButton";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>ReactTimer</h1>
+
+      <AButton>Stop Timers</AButton>
+    </header>
+  );
+}
