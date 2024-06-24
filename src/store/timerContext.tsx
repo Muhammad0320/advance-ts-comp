046508343b90debd7ffaf2fec934186832eb7412
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-24 05:26:50 +0100

    chore: update timers context

--- src/store/timerContext.tsx
+++ src/store/timerContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useContext } from "react";
+import { createContext, ReactNode, useContext, useReducer } from "react";
 import Timer from "../components/Timer";
 
 type Timer = {
@@ -14,29 +14,105 @@ type TimerType = {
 type TimerContextType = TimerType & {
   addTimer(timer: Timer): void;
   stopTimer(): void;
-  deleteTimer(): void;
+  startTimer(): void;
 };
 
 const TimerContext = createContext<TimerContextType | null>(null);
+
+
+
+
+
+type AddTimerAction = { type: 'ADD_TIMER', payload: Timer };
+
+type  StartTimerAction = {  type: 'START_TIMER' }
+type  StopTimerAction = {  type: 'STOP_TIMER' }
+
+type Actions = AddTimerAction | StopTimerAction | StartTimerAction;
+
+
+    const Initialstate: TimerType  = {
+
+        isRunning: false,
+        timers: []
+
+};
+
+const reducer = (state: TimerType, actions: Actions): TimerType => {
+
+        if (actions.type ===  'ADD_TIMER')  { 
+        
+        return { 
+        
+            ...state,
+            
+           timers: [ ...state.timers,  {
+            name: actions.payload.name,
+            duration: actions. payload.duration
+        
+        
+                    } ]
+
+
+        }
+
+          };
+
+          if(actions.type === 'START_TIMER') {
+
+            return {
+
+                ...state,
+                isRunning: true
+
+            }
+
+          }
+
+          if(actions.type === 'STOP_TIMER') {
+
+            return {
+
+                ...state,
+                isRunning: false
+
+            }
+
+          }
+
+
+
+    return state;
+
+}
+
 
 export const TimerContextProvider = function ({
   children,
 }: {
   children: ReactNode;
 }) {
+
+    const [ currrentTimer, dispatch ] = useReducer(reducer, Initialstate);
+
   const ctxVal: TimerContextType = {
-    timers: [],
-    isRunning: false,
+    timers: currrentTimer.timers,
+    isRunning: currrentTimer.isRunning,
     addTimer(timer) {
-      // ....
+        
+        dispatch({  type: 'ADD_TIMER', payload: timer})
+
     },
 
     stopTimer() {
-      // ...
+        
+        dispatch( { type: 'STOP_TIMER' })
+
     },
 
-    deleteTimer() {
-      // ...
+    startTimer() {
+
+     dispatch( { type: 'START_TIMER' })
     },
   };
 
