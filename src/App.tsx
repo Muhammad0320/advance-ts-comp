81837e7669f599c64b3d337cfc4b1f498a6c8834
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-24 04:41:15 +0100

    çhore:  update app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
-import { useRef } from "react";
-import Form, { FormHandle } from "./components/Form";
-import Input from "./components/Inputs";
-import Button from "./components/Buttons";
-import AButton from "./components/AButton";
+import AddTimer from "./components/AddTimer.tsx";
+import Header from "./components/Header.tsx";
+import Timers from "./components/Timers.tsx";
 
 function App() {
-  const formRef = useRef<FormHandle>(null);
-
-  const handleSave = (data: unknown) => {
-    const formData = data as { name: string; age: string };
-
-    console.log(formData);
-
-    formRef.current?.clear();
-  };
-
   return (
-    <main>
-      <Form onSave={handleSave}>
-        <Input label="Name" id="name" type="string" />
-        <Input label="Age" id="age" type="number" />
-        <p>
-          <AButton>Save</AButton>
-        </p>
-      </Form>
-    </main>
+    <>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </>
   );
 }
 
