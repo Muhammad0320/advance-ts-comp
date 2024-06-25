2675d0bf5c7c5210ea7b7416ee4564f6e92c565d
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-25 03:43:53 +0100

    chore:  uodate header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,17 @@ import { useTimer } from "../store/timerContext";
 import AButton from "./UI/AButton";
 
 export default function Header() {
-  const { isRunning } = useTimer();
+  const { isRunning, startTimer, stopTimer } = useTimer();
 
   return (
     <header>
       <h1>ReactTimer</h1>
 
-      <AButton> {isRunning ? "Stop Timer" : "Start timer"} </AButton>
+      <AButton onClick={isRunning ? stopTimer : startTimer}>
+        {" "}
+        {isRunning ? "Stop Timer" : "Start timer"}{" "}
+      </AButton>
     </header>
   );
 }
+
